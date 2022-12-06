8c361bee6ce6b0c2c2786a5b94302ec50122bf7e
Imartino4 <ignacio.martacos@gmail.com>
2022-12-06 13:58:32 +0000

    Task 4 - missing app.py file

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Create app """
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+try:
+    host = os.getenv('HBNB_API_HOST')
+except:
+    host = '0.0.0.0'
+try:
+    port = os.getenv('HBNB_API_PORT')
+except:
+    port = 5000
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    """Close"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host=host, port=port, threaded=True, debug=True)
