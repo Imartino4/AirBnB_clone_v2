8b5005f1bcbec9b5b81bb0c8c1e29026a855f718
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2022-12-09 17:29:01 -0300

    fix places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask api module for places"""
+"""api module for places"""
 from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models.place import Place
