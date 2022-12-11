d3b030b4b12dde3088fc9ec835eab4c832fac8f1
Imartino4 <ignacio.martacos@gmail.com>
2022-12-11 15:16:10 +0000

    Cors - origins fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,7 +15,7 @@ port = int(getenv('HBNB_API_PORT', '5000'))
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
-cors = CORS(app, resources={r"/*": {"origins": "http://localhost:5000"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 # Comparto toda la app, no es mejor solo /api/*?
 
 
